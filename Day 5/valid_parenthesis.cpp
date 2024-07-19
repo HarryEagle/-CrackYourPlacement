6b24c41af8caa8cab138e4b8a99a06cc551dfcae
HarryEagle <93284967+HarryEagle@users.noreply.github.com>
2024-07-19 22:04:23 +0530

    Update valid_parenthesis.cpp

--- Day 5/valid_parenthesis.cpp
+++ Day 5/valid_parenthesis.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(auto it : s){
+            if(it == '(' || it == '{' || it == '['){
+                st.push(it);
+            }
+
+            else{
+                if(st.empty() || (st.top() == '(' && it != ')') || 
+                   (st.top() == '{' &&  it != '}') 
+                   || (st.top() == '['  && it != ']')){
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
